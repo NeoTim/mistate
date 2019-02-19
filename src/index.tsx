e29eac4817236a7070f7e85bfdc7f60ded30a02f
forsigner <forsigner@gmail.com>
2019-02-19 17:39:54 +0800

    fix: fix update store

--- src/index.tsx
+++ src/index.tsx
@@ -8,14 +8,17 @@ function create<S>(init: S) {
   const updaters: Array<Updater<S>> = []
   const Box = class extends React.Component<Props<S>> {
     state: S = state
-    update = (fn: Set<S>, cb: CB<S>) => this.setState((prev: S) => fn(prev), () => cb(this.state))
+    update = (fn: Set<S>, cb: CB<S>) =>
+      this.setState((prev: S) => fn(prev), () => cb((state = this.state)))
     shouldComponentUpdate = (_: Props<S>, nextState: S) => {
       const selector = this.props.selector ? this.props.selector : (s: S) => s
       return !equal(selector(this.state), selector(nextState))
     }
     componentDidMount = () => updaters.push(this.update)
     componentWillUnmount = () => updaters.splice(updaters.indexOf(this.update), 1)
-    render = () => this.props.children(this.state)
+    render = () => {
+      return this.props.children(this.state)
+    }
   }
   return {
     get<P>(selector: (state: S) => P, renderFn?: RenderFn<P>) {
@@ -24,13 +27,17 @@ function create<S>(init: S) {
     },
     set: (next: Set<S> | Partial<S>): Promise<S> =>
       new Promise(resolve => {
+        let resolved = false
         if (!updaters.length) {
           state = Object.assign({}, state, typeof next === 'function' ? next(state) : next)
           return resolve(state)
         }
-        updaters.forEach((update, i) => {
+        updaters.map(update => {
           update(typeof next === 'function' ? next : () => next, s => {
-            if (updaters.length === i + 1) resolve((state = s))
+            if (!resolved) {
+              resolved = true
+              resolve(s)
+            }
           })
         })
       }),
